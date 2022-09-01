c1192cf4d3b32cc1ac1ff91e9bbcdbcd857ffb8a
witorsather <satherws@gmail.com>
2022-08-31 21:13:19 -0300

    finally

--- AtividadeOrdenacao/src/main/Main.java
+++ AtividadeOrdenacao/src/main/Main.java
@@ -18,22 +18,30 @@ public class Main {
 
         long timeInicioOrdenacao = System.nanoTime();//pega o inicio do tempo com vetor desordenado
 
-        String[] vetMerge = vetorDicEnglish.clone(); //fazendo clone do dicionário sem ordenaçaõ
-        String[] vetQuick = vetorDicEnglish.clone();//fazendo clone do dicionário sem ordenaçaõ
-        String[] vetBubble = vetorDicEnglish.clone();//fazendo clone do dicionário sem ordenaçaõ
-        String[] Insertion = vetorDicEnglish.clone();//fazendo clone do dicionário sem ordenaçaõ
-
-        //Sort.mergeSort(vetMerge, 0, vetMerge.length - 1);//ordenando vetor pelo mergeSort
-        //Sort.quick(vetQuick, 0, vetQuick.length - 1);// ordenando vetor pelo quickSort
-        Sort.BubbleSort(vetBubble);//ordenando vetor pelo bubble
-        //Sort.InsertionSort(Insertion);//ordenando vetor pelo InsertionSort
-
+        //String[] vetMerge = vetorDicEnglish.clone(); //fazendo clone do dicionário sem ordenaçaõ
+        String[] vetMerge = vetorDicportuges.clone(); //fazendo clone do dicionário sem ordenaçaõ
+        //String[] vetQuick = vetorDicEnglish.clone();//fazendo clone do dicionário sem ordenaçaõ
+        //String[] vetQuick = vetorDicportuges.clone();//fazendo clone do dicionário sem ordenaçaõ
+        String[] vetBubble = vetorDicportuges.clone();//fazendo clone do dicionário sem ordenaçaõ
+        //String[] vetBubble = vetorDicEnglish.clone();//fazendo clone do dicionário sem ordenaçaõ
+        //String[] Insertion = vetorDicEnglish.clone();//fazendo clone do dicionário sem ordenaçaõ
+        String[] Insertion = vetorDicportuges.clone();//fazendo clone do dicionário sem ordenaçaõ
+        //String[] vetSelectionSort = vetorDicEnglish.clone();//fazendo clone do dicionário sem ordenaçaõ
+        String[] vetSelectionSort = vetorDicportuges.clone();//fazendo clone do dicionário sem ordenaçaõ
+        
+       //Sort.mergeSort(vetMerge, 0, vetMerge.length - 1);//ordenando vetor pelo mergeSort
+       //Sort.quick(vetQuick, 0, vetQuick.length - 1);// ordenando vetor pelo quickSort
+       Sort.BubbleSort(vetBubble);//ordenando vetor pelo bubble
+       //Sort.InsertionSort(Insertion);//ordenando vetor pelo InsertionSort
+       //Sort.quickSort(vetQuick, 0, vetQuick.length - 1);
+       //Sort.SelectionSort(vetSelectionSort);
+       
         long timeFimOrdenacao = System.nanoTime();//pega o final do tempo com vetor ordenado
 
-        for (int i = 0; i < vetBubble.length; i++) {
+        //for (int i = 0; i < vetBubble.length; i++) {
 
-            System.out.println(vetBubble[i]);
-        }
+            //System.out.println(vetBubble[i]);
+       // }
 
         System.out.println("");
 
@@ -45,14 +53,14 @@ public class Main {
         /// Busca Binaria
         System.out.println("Busca Binária!");
         long timeInicoBuscaBinaria = System.nanoTime();
-        System.out.println("" + Buscas.buscaBinaria("q", vetBubble));
+        System.out.println("" + Buscas.buscaBinaria("q", Insertion));
         long timeFimBuscaBinaria = System.nanoTime();
         System.out.println("Tempo de execução Busca Binária!" + "\t" + (timeFimBuscaBinaria - timeInicoBuscaBinaria));
 
         //// Busca Sequencial
         System.out.println("Busca Sequencial!");
         long timeInicoBuscaSequencial = System.nanoTime();
-        System.out.println("" + Buscas.buscaSequencial("q", vetBubble));
+        System.out.println("" + Buscas.buscaSequencial("q", Insertion));
         long timeFimBuscaSequencial = System.nanoTime();
         System.out.println("Tempo de execução Busca Sequancial!" + "\t" + (timeFimBuscaSequencial - timeInicoBuscaSequencial));
     }
--- AtividadeOrdenacao/src/sort/Sort.java
+++ AtividadeOrdenacao/src/sort/Sort.java
@@ -2,6 +2,33 @@ package sort;
 
 public class Sort {
 
+// Neste método a classificação é efetivada por seleção sucessiva do menor valor
+// de chave contido no vetor "A".
+// A cada passo é feita uma varredura do segmento do vetor com os elementos
+// ainda não selecionados e determinado aquele elemento de menor valor, o qual é
+// colocado, por troca, em sua posição definitiva no vetor classificado e o processo
+// repetido para o segmento que contém os elementos ainda não selecionados.
+public static void SelectionSort(String[] vet) {
+        for (int i = 0; i < vet.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < vet.length; j++) {
+                //se o próximo for menor que o atual
+                if (vet[j].length() < vet[index].length()) {
+                    index = j;
+                }
+            }
+             //se o próximo for menor que o atual index recebe o próximo e vai pro valor atual
+             // e o atual recebe o valor do próximo
+            String smallerNumber = vet[index];
+            vet[index] = vet[i];
+            vet[i] = smallerNumber;
+    
+        }
+    }
+
+    // O BubbleSort troca repetidamente elementos adjacentes que não estão em ordem até que toda lista de itens
+    // esteja em sequência. Dessa maneira, os itens flutuam na lista conforme os seus valores, indo do maior (no caso 
+    // de ordenação crescente) pro final ao fim de cada iteração.
     public static void BubbleSort(String[] B) {
 
         String aux = "";
@@ -9,11 +36,13 @@ public class Sort {
         for (int i = 0; i < B.length; i++) {
 
             for (int j = 0; j < B.length - 1; j++) {
-
+                // se o atual for maior que o próximo atual recebe o valor do próximo
                 if (B[j].length() > B[j + 1].length()) {
-
+                    
                     aux = B[j];
+                    // atual pega o valor do próximo
                     B[j] = B[j + 1];
+                    // próximo recebe o valor do atual
                     B[j + 1] = aux;
 
                 }
@@ -23,7 +52,9 @@ public class Sort {
         }
 
     }
-
+    
+    // O InsertionSort varre a lista repetidamente e, a cada vez, insere um item da sequência desordenada na posição correta.
+    // lembra o BubbleSort só que em vez de olhar o da frente olha o de trás
     public static void InsertionSort(String[] C) {
 
         String aux;
@@ -31,7 +62,7 @@ public class Sort {
         for (int i = 0; i < C.length; ++i) {
 
             int j = i;
-
+            // lembra o BubbleSort só que em vez de olhar o da frente olha o de trás e troca
             while (j > 0 && C[j - 1].length() > C[j].length()) {
 
                 aux = C[j];
@@ -43,7 +74,9 @@ public class Sort {
         }
 
     }
-
+    
+    // O MergeSort realiza a ordenação dividindo uma sequência de elementos em sequencias menores recursivamente,
+    // que serão ordenadas e depois serão combinados de forma ordenada.
     public static void mergeSort(String[] a, int from, int to) {
         if (from == to) {
 
@@ -131,16 +164,55 @@ public class Sort {
         return (i + 1);
     }
 
-    /* function to implement quick sort */
-    public static void quick(String a[], int start, int end) /* a[] = array to be sorted, start = Starting index, end = Ending index */ {
+    // O QuickSort trabalha com o princípio da divisão-e-conquista. Primeiramente, ela divide a lista de itens em duas 
+    // sub-listas com base em um elemento pivô. Todos os elementos na primeira sub-lista são dispostos de maneira que 
+    // sejam menores do que o pivô, enquanto que todos os elementos na segunda sub-lista são dispostos para serem 
+    // maiores que o pivô. O mesmo processo de particionamento e organização é executado repetidamente nas sub-listas
+    // resultantes até que toda a lista seja organizada.
+//   public static void quick(String a[], int start, int end) /* a[] = array to be sorted, start = Starting index, end = Ending index */ {
 
-        if (start < end) { //p is the partitioning index
+//        if (start < end) { //p is the partitioning index
 
-            String p = String.valueOf(partition(a, start, end));  //p is the partitioning index  
-            quick(a, start, p.length() - 1);
-            quick(a, p.length() + 1, end);
+//            String p = String.valueOf(partition(a, start, end));  //p is the partitioning index  
+//            quick(a, start, p.length() - 1);
+//            quick(a, p.length() + 1, end);
 
+//        }
+//    }
+
+    // O QuickSort trabalha com o princípio da divisão-e-conquista. Primeiramente, ela divide a lista de itens em duas 
+    // sub-listas com base em um elemento pivô. Todos os elementos na primeira sub-lista são dispostos de maneira que 
+    // sejam menores do que o pivô, enquanto que todos os elementos na segunda sub-lista são dispostos para serem 
+    // maiores que o pivô. O mesmo processo de particionamento e organização é executado repetidamente nas sub-listas
+    // resultantes até que toda a lista seja organizada.
+    public static void quickSort(String v[], int esquerda, int direita) {
+        int esq = esquerda; //inicio do vetor 
+        int dir = direita; //fim do vetor = tamanho do vetor
+        int pivo = v[(esq + dir) / 2].length();
+        String troca;
+
+        while (esq <= dir) {
+            while (v[esq].length() < pivo) {
+                esq = esq + 1;
+            }
+            while (v[dir].length() > pivo) {
+                dir = dir - 1;
+            }
+            if (esq <= dir) {
+                troca = v[esq];
+                v[esq] = v[dir];
+                v[dir] = troca;
+                esq = esq + 1;
+                dir = dir - 1;
+            }
         }
-    }
+        if (dir > esquerda) {
+            quickSort(v, esquerda, dir);
+        }
 
+        if (esq < direita) {
+            quickSort(v, esq, direita);
+        }
+
+    }
 }
